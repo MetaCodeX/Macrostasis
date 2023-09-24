c08dcb53748d659085c2dc8214ac17ecbdaa49ef
Carlos Eduardo Juarez RIcardo <102832240+MetaCodeX@users.noreply.github.com>
2023-09-24 07:50:17 -0600

    New Pages!

--- scripts.js
+++ scripts.js
@@ -1,29 +1,29 @@
 window.onload = function() {
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    document.body.style.zoom = "60%"; 
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      document.body.style.zoom = "60%"; 
+    }
   }
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  var videoBackground = document.getElementById('videoBackground');
-  videoBackground.addEventListener('ended', function() {
-    this.currentTime = 0;
-    this.play();
-  }, false);
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  var miAudio = document.getElementById('miAudio');
-  miAudio.volume = 0.3; // Establece el volumen al 50% (0.0 - 1.0)
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  var miAudio = document.getElementById('miAudio');
-
-  document.addEventListener('click', function() {
-    miAudio.play();
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var videoBackground = document.getElementById('videoBackground');
+    videoBackground.addEventListener('ended', function() {
+      this.currentTime = 0;
+      this.play();
+    }, false);
   });
-});
-function cargarPagina(pagina) {
-  document.getElementById('contenido').src = pagina;
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var miAudio = document.getElementById('miAudio');
+    miAudio.volume = 0.3; // Establece el volumen al 50% (0.0 - 1.0)
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var miAudio = document.getElementById('miAudio');
+  
+    document.addEventListener('click', function() {
+      miAudio.play();
+    });
+  });
+  function cargarPagina(pagina) {
+    document.getElementById('contenido').src = pagina;
 }
