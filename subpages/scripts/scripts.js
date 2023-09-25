0c6c01f7cc6c7d0a45cfc20842a137592efcf622
Carlos Eduardo Juarez RIcardo <102832240+MetaCodeX@users.noreply.github.com>
2023-09-24 20:10:50 -0600

    MidJourney #3

--- subpages/scripts/scripts.js
+++ subpages/scripts/scripts.js
@@ -9,28 +9,3 @@ function cargarPagina(pagina) {
   event.preventDefault(); // Evitar que la página se recargue
 }
 
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
-
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.style.transform = `translateX(${100 * (i - index)}%)`;
-  });
-  currentSlide = index;
-}
-
-function nextSlide() {
-  if (currentSlide < slides.length - 1) {
-    showSlide(currentSlide + 1);
-  }
-}
-
-function prevSlide() {
-  if (currentSlide > 0) {
-    showSlide(currentSlide - 1);
-  }
-}
-
-// Controladores de eventos (puedes usar botones, teclas, etc.)
-document.getElementById('nextBtn').addEventListener('click', nextSlide);
-document.getElementById('prevBtn').addEventListener('click', prevSlide);
